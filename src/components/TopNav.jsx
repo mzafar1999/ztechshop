97bc02f055bcee7cce8c26947c4fb19647bc5ef7
Muhammae Zafar <58272301+mzafar1999@users.noreply.github.com>
2022-02-28 22:43:46 +0500

    Top Nav created

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AiOutlineMail, AiFillPhone } from 'react-icons/ai';
+import { GrFacebook, GrInstagram, GrTwitter, GrLinkedin } from 'react-icons/gr';
+
+const Wrapper = styled.div`
+	background-color: #212934;
+	height: 45px;
+`;
+const Container = styled.div``;
+const ContactWrapper = styled.div``;
+const Text = styled.p`
+	color: white;
+	font-size: 12px;
+	padding-top: 12px;
+`;
+
+const SocialWrapper = styled.div``;
+const Social = styled.div`
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	margin-right: 10px;
+`;
+
+const TopNav = () => {
+	return (
+		<Wrapper>
+			<Container className="container d-flex align-items-center justify-content-between">
+				<ContactWrapper className="d-flex ">
+					<Social style={{ color: 'white' }}>
+						<AiOutlineMail />
+						<Text className="mx-2">z-tech@gmail.com</Text>
+					</Social>
+					<Social style={{ color: 'white' }}>
+						<AiFillPhone />
+						<Text className="mx-2">0304 943 6671</Text>
+					</Social>
+				</ContactWrapper>
+				<SocialWrapper className="d-flex">
+					<Social>
+						<GrFacebook className="mx-2" style={{ color: 'white' }} />
+					</Social>
+					<Social>
+						<GrInstagram className="mx-2" style={{ color: 'white' }} />
+					</Social>
+					<Social>
+						<GrTwitter className="mx-2" style={{ color: 'white' }} />
+					</Social>
+					<Social>
+						<GrLinkedin className="mx-2" style={{ color: 'white' }} />
+					</Social>
+				</SocialWrapper>
+			</Container>
+		</Wrapper>
+	);
+};
+
+export default TopNav;

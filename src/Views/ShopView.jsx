fb5f30b49ba543f7e50a148dccd2e551cc15456f
Muhammae Zafar <58272301+mzafar1999@users.noreply.github.com>
2022-03-11 07:37:04 +0500

    updated

--- src/Views/ShopView.jsx
+++ src/Views/ShopView.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import CardComp from "../components/CardComp";
 export const ContainerFluid = styled.section``;

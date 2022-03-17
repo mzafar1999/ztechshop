04f3a7b76b69687d1ff45b56f58256f7879fb4b8
Muhammae Zafar <58272301+mzafar1999@users.noreply.github.com>
2022-03-17 07:50:44 -0400

    updated all products

--- src/admin/views/AllProducts.jsx
+++ src/admin/views/AllProducts.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Box, Title } from '../../components/styledComponents'
 import SingleProduct from '../components/SingleProduct'
 import {products} from '../products'
 const Container = styled.div`
-    position: relative;
     width: 70vw;
-    height: 98vh;
+    height: 86vh;
     overflow-y: scroll;
     transform: translateX(35%); /* Equal to translate(10px) */
 `
+const Box = styled.div`
+    overflow: hidden;
+`
+const Title = styled.h1`
+    position: relative;
+    left: 160px;
+`
 
 const AllProducts = () => {
-    console.log(products);
   return (
-    <Container className='border mt-2 mx-4 p-2'>
+    <Box className=' mt-2 mx-4 p-2'>
         <Title className='text-center'>AllProducts</Title>
-        <Box className='p-4 m-3 border'>
+        <Container className='p-4  border '>
         { products.map((product) => {
             return <SingleProduct {...product} />
         })}
-        </Box>
-    </Container>
+        </Container>
+    </Box>
   )
 }
 
